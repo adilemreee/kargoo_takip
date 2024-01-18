1432eb88797e2cc171e6baf7371852791009db21
Adil Emre <90987005+adilemreee@users.noreply.github.com>
2024-01-18 18:53:25 +0300

    Initial Commit

---
+++ kargoo_takip/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  kargoo_takip
+//
+//  Created by Adil Emre Karayürek on 18.01.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
